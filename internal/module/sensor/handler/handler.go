a7f7ab5920df6d2109990dfc3dc54db1df15eb68
Andri Muhayat <muhayatandri@gmail.com>
2024-01-04 03:29:53 +0700

    create hadler sensor module

---
+++ internal/module/sensor/handler/handler.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	"github.com/labstack/echo/v4"
+	"log"
+	"net/http"
+	"sensor-streaming/config"
+	"sensor-streaming/internal/module/sensor/usecase"
+	"sensor-streaming/internal/platform/httpengine/httpresponse"
+)
+
+type Handler struct {
+	UseCase usecase.IUseCase
+}
+
+func (h Handler) GenerateStream(c echo.Context) error {
+	request, err := config.MappingRequest(c)
+	if err != nil {
+		return httpresponse.ResponseWithError(c, http.StatusInternalServerError, err.Error())
+	}
+
+	err = h.UseCase.StreamingData(request)
+	if err != nil {
+		log.Println("{Sensor}{GenerateStream}{Error} : ", err)
+		return httpresponse.ResponseWithError(c, http.StatusBadRequest, httpresponse.ErrorBadRequest.Message)
+	}
+
+	return httpresponse.ResponseWithJSON(c, http.StatusOK, httpresponse.ResponseSuccess(http.StatusOK, "success", nil))
+}
+
+func (h Handler) Health(c echo.Context) error {
+	return httpresponse.ResponseWithJSON(c, http.StatusOK, "ok")
+}
+
+func NewHandler(useCase usecase.IUseCase) Handler {
+	return Handler{
+		UseCase: useCase,
+	}
+}
---
+++ internal/module/sensor/handler/route.go
@@ -0,0 +1,11 @@
+package handler
+
+import "github.com/labstack/echo/v4"
+
+func NewSensorRoute(h Handler, route *echo.Group) {
+	//sensor := route.Group("")
+	//sensor.POST("", h.CreateUser)
+	route.GET("/", h.Health)
+	route.POST("/generate-data", h.GenerateStream)
+	//sensor.PUT("/update/status", h.UpdateUserStatus, middleware.AuthorizeJWT(cfg))
+}
