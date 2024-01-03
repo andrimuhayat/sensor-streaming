cea70dc297e9c52a3dea451fdcae8875d7f8d35a
Andri Muhayat <muhayatandri@gmail.com>
2024-01-04 03:32:53 +0700

    add helper util

---
+++ internal/platform/util/help.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"github.com/mitchellh/mapstructure"
+	"strconv"
+)
+
+func DecoderConfig(req interface{}) *mapstructure.DecoderConfig {
+	config := &mapstructure.DecoderConfig{
+		ErrorUnused: true,
+		Result:      &req,
+		TagName:     "json",
+	}
+	return config
+}
+
+func ExpectedInt(v interface{}) int {
+	var result int
+	switch v.(type) {
+	case int:
+		result = v.(int)
+	case float64:
+		result = int(v.(float64))
+	case string:
+		result, _ = strconv.Atoi(v.(string))
+	}
+	return result
+}
