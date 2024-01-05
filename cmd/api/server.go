b78e817a9bcef82f00789c49a05bd80b16e62a72
Andri Muhayat <muhayatandri@gmail.com>
2024-01-05 08:54:42 +0700

    add mqtt connection and test stream data

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -5,15 +5,25 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/spf13/viper"
+	echoSwagger "github.com/swaggo/echo-swagger"
+	docs "sensor-streaming/docs" // Import the docs
 	"sensor-streaming/internal/module/sensor"
 	"sensor-streaming/internal/platform/app"
 	module "sensor-streaming/internal/platform/common"
 	"sensor-streaming/internal/platform/httpengine"
 	"sensor-streaming/internal/platform/httpengine/echoserver"
+	"sensor-streaming/internal/platform/messagebroker/mqqt"
 	internalMdw "sensor-streaming/internal/platform/middleware"
-	"sensor-streaming/internal/platform/storage"
-	"sensor-streaming/internal/platform/storage/migration"
+	"sync"
 )
+
+func RunConsumer(wg *sync.WaitGroup, f func()) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		f()
+	}()
+}
 
 // Server httpengine service
 type Server struct {
@@ -36,11 +46,6 @@ func NewServer() (*Server, error) {
 		return nil, err
 	}
 
-	err = server.initInternalDependency(&appConfig)
-	if err != nil {
-		return nil, err
-	}
-
 	server.Router = echoRouter
 	server.Run(appConfig)
 
@@ -52,45 +57,22 @@ func (s *Server) Run(config app.Config) {
 	s.AppRouter.Use(internalMdw.PanicException)
 	s.AppRouter.Use(middleware.RequestID())
 
+	docs.SwaggerInfo.Title = "Sensor Streaming(A) API"
+	docs.SwaggerInfo.Description = "This is a swagger for Sensor Service."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "0.0.0.0:8081"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+	s.AppRouter.GET("/swagger/*", echoSwagger.WrapHandler)
 	s.initModuleDependency(&config)
 
 	s.Router.SERVE(config.App.Port)
-}
-
-func (s *Server) initInternalDependency(appConfig *app.Config) error {
-	var err error
-
-	sqlConfig := storage.SQLConfig{
-		DriverName:            appConfig.SQL.DriverName,
-		ServiceName:           appConfig.App.Name,
-		Host:                  appConfig.SQL.Host,
-		Port:                  appConfig.SQL.Port,
-		Username:              appConfig.SQL.Username,
-		Password:              appConfig.SQL.Password,
-		Charset:               appConfig.SQL.Charset,
-		DBName:                appConfig.SQL.DbName,
-		MaxOpenConnection:     appConfig.SQL.MaxOpenConnection,
-		MaxIdleConnection:     appConfig.SQL.MaxIdleConnection,
-		MaxLifetimeConnection: appConfig.SQL.MaxLifetimeConnection,
-	}
-	DB, err := storage.NewMysqlClient(&sqlConfig)
-	if err != nil {
-		return err
-	}
-	s.DB = DB
-
-	if appConfig.SQL.DbMigrate {
-		migration.MigrationRubenv(DB)
-	}
-
-	return nil
 }
 
 func (s *Server) initModuleDependency(appConfig *app.Config) module.Dependency {
 	var (
 		dependency module.Dependency
 	)
-
+	dependency.MqttClient = mqqt.Connect("pub", appConfig.Mqtt)
 	sensor.StartService(dependency, s.AppRouter)
 	return dependency
 }
--- internal/platform/common/common.go
+++ internal/platform/common/common.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/jmoiron/sqlx"
 	"sensor-streaming/internal/module/sensor/repository"
 	"sensor-streaming/internal/module/sensor/usecase"
@@ -12,4 +13,6 @@ type Dependency struct {
 	SensorRepository repository.IRepository
 	//usecase
 	SensorUseCase usecase.IUseCase
+
+	MqttClient mqtt.Client
 }
